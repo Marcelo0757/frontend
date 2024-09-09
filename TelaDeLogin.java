6a54fc11282d096c8cf799db390229ceb019a96e
Marcelo0757 <marcelo1965.campos2024@gmail.com>
2024-09-09 09:23:21 -0300

    Add files via upload

--- TelaDeLogin.java
+++ TelaDeLogin.java
@@ -1,8 +1,9 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.sql.*;
 
-public class TelaDeLogin extends JFrame 
+public class TelaDeLogin extends JFrame
 {
     private final JLabel lblLogin;
     private final JTextField txtLogin;
@@ -13,7 +14,7 @@ public class TelaDeLogin extends JFrame
     private final JButton btnEntrar;
 
     private final JLabel lblNotificacoes;
-    
+
     public TelaDeLogin()
     {
         super("Tela de Login");
@@ -25,7 +26,7 @@ public class TelaDeLogin extends JFrame
         txtLogin = new JTextField(10);
         add(txtLogin);
 
-        lblSenha = new JLabel("Senha");
+        lblSenha = new JLabel("Senha:");
         add(lblSenha);
 
         txtSenha = new JPasswordField(10);
@@ -34,18 +35,34 @@ public class TelaDeLogin extends JFrame
         btnEntrar = new JButton("Entrar");
         add(btnEntrar);
 
-        add(new JLabel(""));
-        lblNotificacoes = new JLabel 
-        ("Notificacões", SwingConstants.CENTER);
+        lblNotificacoes = new JLabel("Notificações", SwingConstants.CENTER);
         add(lblNotificacoes);
 
-        setSize(150,200);
+        btnEntrar.addActionListener(
+            new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent event) {
+                    try {
+                        Connection conexao = MySQLConnector.conectar();
+                        String strSqlLogin = "select * from `db_senac`.`tbl_senac` where `email` = '" + txtLogin.getText() + "' and `senha` = '" + txtSenha.getPassword() + "';";
+                        Statement stmSqlLogin = conexao.createStatement();
+                        ResultSet rstSqlLogin = stmSqlLogin.executeQuery(strSqlLogin);
+                        rstSqlLogin.next();
+                        stmSqlLogin.close();
+                        lblNotificacoes.setText("Conectado com sucesso!!!");
+                    } catch (Exception e) {
+                        lblNotificacoes.setText("Não foi possível encontrar o login e/ou senha digitados/informados! Por favor, verifique e tente novamente. Veja o erro: " + e);
+                    }
+                }
+            }
+        );
+
+        setSize(150, 200);
         setVisible(true);
-        MySQLConnector.conectar();
     }
 
-        public static void main(String[] args) {
-            TelaDeLogin appTelaDeLogin = new TelaDeLogin();
-            appTelaDeLogin.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        }
+    public static void main(String[] args) {
+        TelaDeLogin appTelaDeLogin = new TelaDeLogin();
+        appTelaDeLogin.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
 }
